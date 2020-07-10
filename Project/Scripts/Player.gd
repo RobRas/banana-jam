d859c9352392999b87d0d69ebd15190610502f46
Robert <bobbyras90@gmail.com>
2020-07-10 13:14:10 -0700

    Player can move and look towards mouse

---
+++ Project/Scripts/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+
+export(int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	look_at(get_global_mouse_position())
+	velocity = Vector2()
+	if Input.is_action_pressed("move_down"):
+		velocity += Vector2(0, speed)
+	if Input.is_action_pressed("move_up"):
+		velocity -= Vector2(0, speed)
+	if Input.is_action_pressed("move_left"):
+		velocity -= Vector2(speed, 0)
+	if Input.is_action_pressed("move_right"):
+		velocity += Vector2(speed, 0)
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
