5d6a1b8bae645ac85bafaaf15e4727c607ce1799
Toribio <nutsforalmonds@outlook.com>
2020-07-10 16:57:20 -0700

    does it work now?

--- Project/Scripts/Level.gd
+++ Project/Scripts/Level.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+export (PackedScene) var Baddie
+var hit_count;
 
 func _on_player_shot(bullet):
 	var current_position = bullet.position
 	add_child(bullet)
 	bullet.position = current_position
+
+
+func _on_Player_hit():
+	hit_count += 1
+
+func _on_BaddieSpawnTimer_timeout():
+	var baddie = Baddie.instance()
+	add_child(baddie)
+	baddie.position
