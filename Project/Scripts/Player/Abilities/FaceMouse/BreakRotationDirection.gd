53cf16b4565a19d50e6db2703d092ab07d24970b
Robert <bobbyras90@gmail.com>
2020-07-10 20:30:24 -0700

    Better one-direction

--- Project/Scripts/Player/Abilities/FaceMouse/BreakRotationDirection.gd
+++ Project/Scripts/Player/Abilities/FaceMouse/BreakRotationDirection.gd
@@ -1,17 +1,13 @@
 extends Node2D
 
 export(bool) var broken
-export(float) var forgiveness = 10
 
 var _broken = false
 var _direction = -1
-var _forgiveness_rad = 0
 
 
 func _ready():
-	_forgiveness_rad = deg2rad(forgiveness)
 	set_broken(broken)
-
 
 func set_broken(broken):
 	if _broken == broken:
@@ -29,11 +25,9 @@ func modify_rotation_angle(delta, angle, total_angle):
 	print(new_angle)
 	
 	if sign(angle) == _direction:
-		new_angle = get_parent().get_turn_speed_rad() * delta * -_direction
-	elif abs(total_angle) < _forgiveness_rad:
-		new_angle = 0
+		return 0
 	
-	return new_angle
+	return angle
 
 func is_broken():
 	return _broken
