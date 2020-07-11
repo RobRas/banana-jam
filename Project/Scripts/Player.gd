a8bef45b72c2a67b05a31b698656cd4e31aaf610
DESKTOP-NPCILDG\jdany <jdanylik@gmail.com>
2020-07-10 21:25:28 -0700

    Audio and Animation changes

--- Project/Scripts/Player.gd
+++ Project/Scripts/Player.gd
@@ -16,6 +16,12 @@ func get_input():
 		velocity -= Vector2(speed, 0)
 	if Input.is_action_pressed("move_right"):
 		velocity += Vector2(speed, 0)
+	if velocity.length()>0:
+		$RocketBooster.stream_paused=false
+		$Sprite.play("Boosting")
+	else:
+		$RocketBooster.stream_paused=true
+		$Sprite.play("Idle")
 
 func _physics_process(_delta):
 	get_input()
