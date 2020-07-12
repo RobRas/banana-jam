9ddc7a88b3145a114c63c03348c31098f1663315
Robert <bobbyras90@gmail.com>
2020-07-11 23:51:33 -0700

    Better sniper

--- Project/Scripts/Guns/MouseHoldAndRelease.gd
+++ Project/Scripts/Guns/MouseHoldAndRelease.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal hold_started()
+signal shot_input(input_value)
 
 export(float) var cooldown = 0.2
 var _current_cooldown = 0.0
@@ -10,28 +12,29 @@ var _current_hold_time = 0.0
 
 export(float) var forgiveness = 0.15
 
-signal shot_input(input_value)
+var _percent_held = 0
 
 
 func _process(delta):
 	_current_cooldown -= delta
 	if Input.is_action_just_pressed("shoot"):
 		_current_hold_time = 0.0
-	
+		_percent_held = 0.0
+		emit_signal("hold_started")
 	if Input.is_action_pressed("shoot") and _current_cooldown <= 0.0:
 			_current_hold_time += delta
+			_percent_held = (_current_hold_time - min_hold_time) / (max_hold_time - min_hold_time)
+			_percent_held = min(_percent_held + forgiveness, 1.0)
 	
 	if Input.is_action_just_released("shoot"):
-		if _current_hold_time < min_hold_time:
-			pass
-		elif _current_hold_time > max_hold_time:
-			pass
-		else:
-			var percent_held = (_current_hold_time - min_hold_time) / (max_hold_time - min_hold_time)
-			percent_held = min(percent_held + forgiveness, 1.0)
+			_percent_held = (_current_hold_time - min_hold_time) / (max_hold_time - min_hold_time)
+			_percent_held = min(_percent_held + forgiveness, 1.0)
 			_current_cooldown = cooldown
 			_current_hold_time = 0.0
-			emit_signal("shot_input", percent_held)
+			emit_signal("shot_input", _percent_held)
+
+func get_percent_held():
+	return _percent_held
 
 func equip():
 	_current_cooldown = cooldown
