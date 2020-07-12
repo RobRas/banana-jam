03e9a2f965b3f7a80562c5ab0314442f2a225d08
Robert <bobbyras90@gmail.com>
2020-07-12 11:36:04 -0700

    Fix sniper more

--- Project/Scripts/Guns/Breaks/SniperKnockback.gd
+++ Project/Scripts/Guns/Breaks/SniperKnockback.gd
@@ -7,6 +7,19 @@ export(Vector2) var minor_knockback = Vector2(300, 1000)
 export(Vector2) var knockback_initial_speed = Vector2(300, 50000)
 export(Vector2) var knockback_duration = Vector2(0.3, 2.5)
 export(NodePath) var shoot_path
+
+export(NodePath) var camera_shake_path
+var camera_shake
+
+export(float) var period = 0.07
+export(Vector2)	var magnitude = Vector2(25, 25)
+export(float) var rot = 7
+export(float) var duration = 0.6
+
+export(float) var break_period = 0.07
+export(Vector2)	var break_magnitude = Vector2(50, 50)
+export(float) var break_rot = 10
+export(float) var break_duration = 0.7
 var _shoot
 
 var _player
@@ -20,6 +33,7 @@ var _previous_speed = 0
 var _knockback_value
 
 func init(player):
+	camera_shake = get_node(camera_shake_path)
 	_knockback_value = minor_knockback
 	_player = player
 	_shoot = get_node(shoot_path)
@@ -28,8 +42,16 @@ func set_broken(broken):
 	_broken = broken
 	if _broken:
 		_knockback_value = knockback_initial_speed
+		camera_shake.period = break_period
+		camera_shake.magnitude = break_magnitude
+		camera_shake.rot = break_rot
+		camera_shake.duration = break_duration
 	else:
 		_knockback_value = minor_knockback
+		camera_shake.period = period
+		camera_shake.magnitude = magnitude
+		camera_shake.rot = rot
+		camera_shake.duration = duration
 
 func _process(delta):
 	_player.additional_velocity -= _direction * _previous_speed
