7f46e078ed97edd60ff778dd71eb6aad12f1c61a
Toribio <5829782+nutsforalmonds@users.noreply.github.com>
2020-07-11 17:42:55 -0700

    fix repair and get Flankers to circle the other way

--- Project/Scripts/Baddie.gd
+++ Project/Scripts/Baddie.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 signal enemy_hit
 signal Baddie_dead
+var dead = false
 
 func _physics_process(_delta):
 	velocity = move_and_slide(velocity)
@@ -14,4 +15,6 @@ func _on_Area2D_area_entered(area):
 	emit_signal("enemy_hit",global_position)
 
 func _on_Baddie_dead(drop_value, drop_position):
-	emit_signal("Baddie_dead", drop_value, drop_position)
+	if not dead:
+		emit_signal("Baddie_dead", drop_value, drop_position)
+		dead = true
