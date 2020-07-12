edb7cb58fb9a9f87019aca83e021f84a5230dcf7
Robert <bobbyras90@gmail.com>
2020-07-12 01:31:15 -0700

    Add sniper cooldown

--- Project/Scripts/Guns/MouseHoldAndRelease.gd
+++ Project/Scripts/Guns/MouseHoldAndRelease.gd
@@ -13,24 +13,29 @@ var _current_hold_time = 0.0
 export(float) var forgiveness = 0.15
 
 var _percent_held = 0
+var _holding = false
 
 
 func _process(delta):
 	_current_cooldown -= delta
 	if Input.is_action_just_pressed("shoot"):
+		if not _current_cooldown <= 0:
+			return
+		_holding = true
 		_current_hold_time = 0.0
 		_percent_held = 0.0
 		emit_signal("hold_started")
-	if Input.is_action_pressed("shoot") and _current_cooldown <= 0.0:
+	if Input.is_action_pressed("shoot") and _holding:
 			_current_hold_time += delta
 			_percent_held = (_current_hold_time - min_hold_time) / (max_hold_time - min_hold_time)
 			_percent_held = min(_percent_held + forgiveness, 1.0)
 	
-	if Input.is_action_just_released("shoot"):
+	if Input.is_action_just_released("shoot") and _holding:
 			_percent_held = (_current_hold_time - min_hold_time) / (max_hold_time - min_hold_time)
 			_percent_held = min(_percent_held + forgiveness, 1.0)
 			_current_cooldown = cooldown
 			_current_hold_time = 0.0
+			_holding = false
 			emit_signal("shot_input", _percent_held)
 
 func get_percent_held():
@@ -38,8 +43,10 @@ func get_percent_held():
 
 func equip():
 	_current_cooldown = cooldown
+	_holding = false
 	set_process(true)
 
 func unequip():
 	_current_hold_time = 0.0
+	_holding = false
 	set_process(false)
